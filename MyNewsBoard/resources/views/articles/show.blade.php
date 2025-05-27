391ba92fd57b5d826b3b69d92ddca802fcf94408
nicol129 <nicegai1313@gmail.com>
2025-05-27 17:57:33 +0900

    MyNewsBoardにユーザー認証機能を追加

--- MyNewsBoard/resources/views/articles/show.blade.php
+++ MyNewsBoard/resources/views/articles/show.blade.php
@@ -9,25 +9,32 @@
 <h2>コメント一覧</h2>
 
 @foreach ($comments as $comment)
-    <p>・{{ $comment->content }}</p>
-    <form action="{{ route('comments.edit', $comment->id) }}" method="GET">
-        @csrf
-        <button type="submit" class="btn btn-primary">編集</button>
-    </form>
+    <div style="display: flex; align-items: center; margin-bottom: 10px;">
+        <p style="margin-right: 20px;">・{{ $comment->content }}</p>
 
-    <form action="{{ route('comments.destroy', $comment->id) }}" method="POST" style="display:inline;">
-        @csrf
-        @method('DELETE')
-        <button type="submit">削除</button>
-    </form>
+        @if (Auth::id() === $comment->user_id)
+        <form action="{{ route('comments.edit', $comment->id) }}" method="GET" style="margin-right: 5px;">
+            @csrf
+            <button type="submit" class="btn btn-primary">編集</button>
+        </form>
+
+        <form action="{{ route('comments.destroy', $comment->id) }}" method="POST" style="display:inline;">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">削除</button>
+        </form>
+        @endif
+    </div>
 @endforeach
 
 <!-- コメント投稿フォーム -->
+@auth
 <form action="{{ route('comments.store', $article['id']) }}" method="POST">
     @csrf
     <textarea name="content" rows="4" placeholder="コメントを入力"></textarea>
     <button type="submit">コメント投稿</button>
 </form>
+@endauth
 
 <div style="margin-top: 2em;">
     <a href="{{ route('articles.index') }}">← 記事一覧に戻る</a>
